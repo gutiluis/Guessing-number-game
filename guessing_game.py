6ae626e0cb48953492634716f55169b7b264d5ce
luis <gutierrez.luis@icloud.com>
2025-06-02 10:22:52 -0700

    Add files via upload

--- guessing_game.py
+++ guessing_game.py
@@ -3,50 +3,68 @@ Python Development Techdegree
 Project 1 - The Number Guessing Game
 --------------------------------
 """
-import random, time, sys
+import random
+import sys
+import time
 
 
 
-def main():
-	start_game()
-	continue_game()
+def select_user():
+	a = input("Select user name: ").strip()
+	return a
 
-def start_game():
+
+def start_game(player_name):
+	print("#"*10 + " "*10 + "#"*10)
+	print("#"*10 + " " + f"Welcome {player_name}!" + " " + "#"*10)
 	print("#"*20)
-	print("          ######################### Welcome Player! ##############################")
 	print("#"*20)
-	print(" "*10, "#"*20)
 	time.sleep(1)
 	random_number = random.randint(1,10)
 	guesses = 0
 	secret_number = True
 	while secret_number != random_number:
 		try:
-			secret_number = int(input("Please guess a number between 1 and 10:    "))
+			secret_number = int(input("Guess a number between 1 and 10: "))
+			if secret_number > 10 or secret_number < 1:
+				print("Only numbers between 1 and 10. Try again...")
+				continue
 			if secret_number < random_number:
-				print("It's higher. Try again...")
+				print("Wrong. It's higher. Try again...")
 				guesses += 1
 			if secret_number > random_number:
-				print("It's lower. Try again...")
+				print("Wrong. It's lower. Try again...")
 				guesses += 1
 		except ValueError as err:
-			print("Value entered not valid. Try again with integers only from 1 through 10.")
+			print("Enter a number from 1 to 10.")
 			print("({})".format(err))
-	secret_number = random_number
-	guesses += 1
-	print("Congratulations it took you",str(guesses),"attempts. The random number is",(secret_number),)
-start_game()
+	else:
+		secret_number = random_number
+		guesses += 1
+		print("Congratulations it took you",str(guesses),"attempts. The random number is",(secret_number),)
+
 
 def continue_game():
-	continue_game = input("Try again?")
-	if continue_game == ("Yes", "y", "yes"):
-		start_game()
-	else: 
-		continue_game == ("no", "n")
-		print("Game Over.")
-		sys.exit()
-continue_game()
+    while True:
+        any_var = input("Play again? ").strip()
+        if any_var.isdigit():
+            print("Enter Yes or No only.")
+            continue
+        if any_var in ["Yes","Y","y","YES", "yes"]:
+            start_game()
+        else:
+            any_var in ["No","N","NO","no","n"]
+            print("Game over.")
+            sys.exit()
+        
+
+def main():
+	a = select_user()
+	start_game(a)
+	continue_game()
 
 
-main()
+
+if __name__ == "__main__":
+	main()
 
